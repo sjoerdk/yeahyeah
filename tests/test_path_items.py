5894d1e35ebb4383fb11ff1b87a2494c28e70f30
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-04-01 10:54:23 +0200

    Reveals bug in path admin

--- tests/test_path_items.py
+++ tests/test_path_items.py
@@ -17,5 +17,27 @@ def test_path_item_plugin(path_item_list):
     assert len(plugin.get_menu_items()) == 2
 
 
+def test_url_pattern_plugin_admin_add__remove_list_record(yeahyeah_instance):
+    runner = CliRunner()
+    path_item_plugin = yeahyeah_instance.plugins[1]
 
+    assert len(path_item_plugin.item_list) == 2
+
+    response = runner.invoke(
+        yeahyeah_instance.admin_cli,
+        "path_items add a_path /test/something/folder"
+    )
+    assert response.exit_code == 0
+    assert len(path_item_plugin.item_list) == 3
+
+    response = runner.invoke(yeahyeah_instance.admin_cli, "path_items list")
+
+    assert response.exit_code == 0
+
+    response = runner.invoke(
+        yeahyeah_instance.admin_cli,
+        "path_items remove a_path".split(" ")
+    )
+    assert response.exit_code == 0
+    assert len(path_item_plugin.item_list) == 2
 
