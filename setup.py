d0aa725633f2d53e4928cef10e43c67b96bb0f6c
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-10-31 10:41:45 +0100

    Fixes requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'pyyaml']
+requirements = ['Click>=6.0', 'pyyaml', 'clockifyclient', 'python-dateutil']
 
 setup_requirements = ['pytest-runner']
 
