4fe450617c54b37d61ca6cc9a464a34b4e85d449
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-11-20 14:38:20 +0100

    Adds department and email switches

--- yeahyeah_plugins/ad_plugin/cli.py
+++ yeahyeah_plugins/ad_plugin/cli.py
@@ -1,12 +1,17 @@
 import sys
 
 import click
+from umcnad.core import UMCNPerson
 
 from yeahyeah.decorators import pass_yeahyeah_context
 from yeahyeah.context import YeahYeahContext
 from yeahyeah.persistence import JSONSettingsFile
-from yeahyeah_plugins.ad_plugin.context import default_settings_file_name, \
-    default_context, ADPluginContext, pass_ad_context
+from yeahyeah_plugins.ad_plugin.context import (
+    default_settings_file_name,
+    default_context,
+    ADPluginContext,
+    pass_ad_context,
+)
 from yeahyeah_plugins.ad_plugin.decorators import handle_umcnad_exceptions
 from yeahyeah_plugins.ad_plugin.translator import find_z_numbers, Translator
 
@@ -41,22 +46,37 @@ def status(context: ADPluginContext):
 def find_z_number(context: ADPluginContext, z_numbers):
     """print name and info for z-number if possible"""
     if not z_numbers:
-        raise click.badparameter('no z-numbers given')
+        raise click.badparameter("no z-numbers given")
     people = context.search_people(list(z_numbers))
     for person in people:
-        click.echo(f'{person} - {person.department}')
+        click.echo(f"{person} - {person.department}")
 
 
 @click.command()
 @handle_umcnad_exceptions
 @pass_ad_context
 @click.argument("input_string", nargs=-1)
-def translate(context: ADPluginContext, input_string):
+@click.option(
+    "--department/--no-department", default=False, help="Print department after name"
+)
+@click.option(
+    "--email/--no-email", default=False, help="Print email after name"
+)
+def translate(context: ADPluginContext, input_string, department, email):
     """replace any z-number in input text with name"""
     # input_string = ''.join(sys.stdin.readlines())
     input_string = " ".join(input_string)
     people = context.search_people(list(find_z_numbers(input_string)))
-    glossary = {x.z_number: str(x) for x in people}
+
+    def person_to_string(person: UMCNPerson):
+        person_string = str(person)
+        if department:
+            person_string += f"({person.department})"
+        if email:
+            person_string += f"({person.email})"
+        return person_string
+
+    glossary = {x.z_number: person_to_string(x) for x in people}
     click.echo(Translator(glossary).process(input_string))
 
 
