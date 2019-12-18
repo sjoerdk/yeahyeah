96c81250c05936dc7c722ffe0edb3ef79cc4ac20
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-12-18 16:58:55 +0100

    Bump version: 0.2.10 → 0.3.0

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sjoerdk/yeahyeah',
-    version='0.2.10',
+    version='0.3.0',
     zip_safe=False,
 )
